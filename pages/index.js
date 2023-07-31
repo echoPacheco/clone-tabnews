abdfa15ca59dca2d6c1cbf4baa6d0e9cc6454d24
echoPacheco <61707361+echoPacheco@users.noreply.github.com>
2023-07-31 23:55:48 +0000

    sdeschamps challenge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Mensagem impactante!!!! 😮😮</h1>
+    return <h1>João, faltam 5 meses!!!! 😮😮
+        <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fdead-cat-gifs&psig=AOvVaw21nLye1YMT468fX8dQABHA&ust=1690933953246000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPC05JmRuoADFQAAAAAdAAAAABAE"></a>
+    </h1> 
 }
 
 export default Home;
