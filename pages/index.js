7e1eaa9d3b128209b6317b54e439c5ac83959dbd
echoPacheco <61707361+echoPacheco@users.noreply.github.com>
2023-08-01 01:40:55 +0000

    removing emotes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>João, faltam 5 meses!!!! 😮😮
+    return <h1>João, faltam 5 meses!!!!
         <br></br>
         <img src="https://media.tenor.com/So_wSVwEzmYAAAAC/cat-died.gif"></img>
     </h1> 
