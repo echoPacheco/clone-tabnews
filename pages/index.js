6d219bf4aef5ea69eb11ed77d75c6f0551c0b693
echoPacheco <61707361+echoPacheco@users.noreply.github.com>
2023-08-01 00:07:53 +0000

    fixing code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
     return <h1>João, faltam 5 meses!!!! 😮😮
-        <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fdead-cat-gifs&psig=AOvVaw21nLye1YMT468fX8dQABHA&ust=1690933953246000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPC05JmRuoADFQAAAAAdAAAAABAE"></a>
+        <br>
+        <img src="https://media.tenor.com/So_wSVwEzmYAAAAC/cat-died.gif"></img>
     </h1> 
 }
 
