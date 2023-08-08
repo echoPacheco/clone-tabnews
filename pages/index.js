ee40bf9edf62cbd30d6d8c03ced978db88cac8fa
echoPacheco <61707361+echoPacheco@users.noreply.github.com>
2023-08-08 19:18:58 +0000

    adding scritps `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 function Home() {
-    return <h1>João, faltam 5 meses!!!!
-        <br></br>
-        <img src="https://media.tenor.com/So_wSVwEzmYAAAAC/cat-died.gif"></img>
-    </h1> 
+  return (
+    <h1>
+      João, faltam 5 meses!!!!
+      <br></br>
+      <img src="https://media.tenor.com/So_wSVwEzmYAAAAC/cat-died.gif"></img>
+    </h1>
+  );
 }
 
 export default Home;
